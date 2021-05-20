68f3ac993e97f45fceccadf1e31ef21abb4adfc4
hoangnv65 <hoangnv65@viettel.com.vn>
2021-05-20 17:55:11 +0700

    switch to Kotlin

---
+++ Medium.kt
@@ -0,0 +1,18 @@
+class Solution {
+    fun countRangeSum(nums: IntArray, lower: Int, upper: Int): Int {
+        return 0
+    }
+
+    fun welcome() {
+        println("Hello World!")
+    }
+}
+
+fun main() {
+    println("Hello World!")
+    val solution = Solution()
+    solution.welcome()
+}
+
+// kotlinc Medium.kt -include-runtime -d Medium.jar
+// java -jar Medium.jar
