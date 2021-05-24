772b368068198148d6c968c158b6696fec99cddf
hoangnv65 <hoangnv65@viettel.com.vn>
2021-05-24 14:21:45 +0700

    update

--- Algo/src/main/kotlin/HardProblems.kt
+++ Algo/src/main/kotlin/HardProblems.kt
@@ -1,30 +1,29 @@
+import java.util.*
+
 class HardSolution {
     /* 327. Count of Range Sum */
     fun countRangeSum(nums: IntArray, lower: Int, upper: Int): Int {
-        val rangSumCounter = 0
-        println(countSubArray(nums, lower, upper))
-        return rangSumCounter
-    }
-
-    private fun countSubArray(array: IntArray, lowerLimit: Int, upperLimit: Int): Int {
-        var counter = 0
-        for (i in 0..array.size-1) {
-            var sum = 0
-            for (j in i..array.size-1) {
-                if (sum + array[j] >= lowerLimit && sum + array[j] <= upperLimit) {
-                    sum += array[j]
-                    counter++
-                } else {
-                    println(sum)
-                    break
-                }
+        var rangeSumCounter = 0
+        val numsInLong = Arrays.stream(nums).asLongStream().toArray()
+        val sumArray = arrayListOf<Long>()
+        for (i in 0..numsInLong.size-1) {
+            var sum = 0L
+            for (j in i..numsInLong.size-1) {
+                sum += nums[j]
+                sumArray.add(sum)
             }
         }
-        return counter
+        for (sum in sumArray) {
+            if (sum in lower..upper){
+                rangeSumCounter++
+            }
+        }
+        return rangeSumCounter
     }
 }
 
 fun main(args: Array<String>) {
     val solution = HardSolution()
-    println(solution.countRangeSum(intArrayOf(-2, 5, -1), -2, 2))
+    // println(solution.countRangeSum(intArrayOf(-2, 5, -1), -2, 2))
+    println(solution.countRangeSum(intArrayOf(0), 0, 0))
 }
