5c43c04a3a4edfe1a9c36ad567436a3c94f7de1b
hoangnv11 <nvhoang191@gmail.com>
2022-03-11 00:28:39 +0700

    codility lesson 2

--- Algo/src/main/kotlin/codility/Codility.kt
+++ Algo/src/main/kotlin/codility/Codility.kt
@@ -1,5 +1,8 @@
 package codility
 
+import java.io.IOException
+import java.io.Reader
+
 class Codility {
     // Task 1: run length encoding
     fun calculateMinOperation(binaryString: String): Int {
@@ -13,6 +16,34 @@ class Codility {
             return divide(number/2, operations+1)
         }
         return divide(number-1, operations+1)
+    }
+}
+
+class SolutionIter(private val reader: Reader): Iterable<Int> {
+    override fun iterator(): Iterator<Int> {
+        val list = arrayListOf<Int>()
+        var charCode: Int
+        var line = StringBuilder("")
+        var s: String
+        var number: Int?
+        try {
+            while (true) {
+                charCode = reader.read()
+                if (charCode == 10 || charCode == -1) {
+                    s = line.toString().trim()
+                    number = s.toIntOrNull()
+                    if (number != null && number <= 1000000000 && number >= -1000000000) list.add(number)
+                    line = StringBuilder("")
+                    if (charCode == -1) break
+                } else {
+                    line.append(charCode)
+                }
+            }
+            reader.close()
+        } catch (e: IOException) {
+
+        }
+        return list.iterator()
     }
 }
 
--- Algo/src/main/kotlin/codility/Training.kt
+++ Algo/src/main/kotlin/codility/Training.kt
@@ -21,6 +21,47 @@ fun findBinaryGap(number: Int): Int {
     return gap
 }
 
+// Lesson 2: Arrays
+// CyclicRotation - 100%
+fun rotateIntArray(array: IntArray, times: Int): IntArray {
+    val arraySize = array.size
+    if (array.isEmpty() || arraySize == 1 || arraySize == times || times == 0) return array
+    val lastIndex = arraySize-1
+    val shiftedArray = IntArray(array.size)
+    val actualTimes = times%arraySize
+    for (idx in array.indices) {
+        val indexAfterShifted = idx+actualTimes
+        val newIndex = if (indexAfterShifted > lastIndex) indexAfterShifted - arraySize else indexAfterShifted
+        shiftedArray[newIndex] = array[idx]
+    }
+    return shiftedArray
+}
+
+// OddOccurrencesInArray - 100%
+fun findUnpairedElement(array: IntArray): Int {
+    val arraySize = array.size
+    if (arraySize == 1) return array[0]
+    array.sort() // sort in-place
+    if (array[arraySize-1] != array[arraySize-2]) return array[arraySize-1]
+    var checkPair = true
+    var previousNumber = array[0]
+    for (idx in 1 until array.size) {
+        if (checkPair) {
+            if (previousNumber == array[idx]) checkPair = false
+            else return previousNumber
+        } else {
+            previousNumber = array[idx]
+            checkPair = true
+        }
+    }
+    return array[arraySize-1]
+}
+
 fun main() {
-    print("Binary gap = ${findBinaryGap(number = 32)}")
+//    print("Binary gap ->  ${findBinaryGap(number = 32)}") // Binary Gap
+//    val shiftedArray = rotateIntArray(array = intArrayOf(3, 8, 9, 7, 6), times = 3)
+//    for (number in shiftedArray) {
+//        println(number)
+//    }
+    print("Unpaired element = ${findUnpairedElement(array = intArrayOf(9, 9, 3))}")
 }
